aa2562afca2429f4fe542ca1840d0fbaeb0bd81e
Prometheusjj7 <gustavomoral367@gmail.com>
2026-07-07 18:59:42 -0300

    Remove partner entries from partners.js
    Removed multiple partner entries from the list.

--- data/partners.js
+++ data/partners.js
@@ -1,9 +1,3 @@
 const partners = [
-    { name: "Crew Blacklist", discount: 10 },
-    { name: "YellowJack", discount: 10 },
-    { name: "Crew Midnight", discount: 10 },
-    { name: "Cassey Dinner", discount: 10 },
-    { name: "Crips", discount: 10 },
-    { name: "Pawnshop", discount: 10 },
-    { name: "Funcionário", discount: 15 }
+//    { name: "Funcionário", discount: 15 }
 ];
